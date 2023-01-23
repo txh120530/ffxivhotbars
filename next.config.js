8f3300118cadb6217c0a8b991e0bf947f73d7b18
Travis Harvey <txh120530@gmail.com>
2023-01-23 01:17:22 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/ffxivhotbars/' : '',
+  assetPrefix: './',
   images: {
     unoptimized: true,
   },
